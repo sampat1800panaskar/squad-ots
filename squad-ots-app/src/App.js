402228b76e068f9374591774133a3dd09d5f4af4
Akash Bhomani Belgaonkar <akash.bhomani@otsconsultingservices.com>
2024-03-20 12:33:30 +0530

    fdsfdsf

--- squad-ots-app/src/App.js
+++ squad-ots-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MainLogic from './cal-logic';
 
 function App() {
   return (
     <div className="App">
-      <<h5>Changes from Git</h5>
+      <h5>Changes from Git</h5>
+      <MainLogic />
     </div>
   );
 }
---
+++ squad-ots-app/src/cal-logic/index.jsx
@@ -0,0 +1,11 @@
+
+const MainLogic = () => {
+
+    return(
+        <>
+            <h4>Akash Logc</h4>
+        </>
+    )
+}
+
+export default MainLogic;
