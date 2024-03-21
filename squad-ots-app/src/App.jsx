085f3ee02dc99c940c4c0d439d94fdf13668f313
Pratik Kutre <pratik.kutre@championx.com>
2024-03-21 15:29:03 +0530

    Calculator using Class Components

---
+++ squad-ots-app/src/App.jsx
@@ -0,0 +1,14 @@
+import MainCal from './component-cal';
+import './component-cal/Cal.css';
+
+function App () {
+    return (
+      <div>
+          <MainCal />
+      </div>
+    )
+  }
+  
+
+export default App;
+
---
+++ squad-ots-app/src/component-cal/index.jsx
@@ -0,0 +1,63 @@
+import { Component } from "react";
+import Result from "./Result";
+import Keypad from "./Keypad";
+
+class MainCal extends Component{
+    state = {
+        result: ""
+      }
+    
+      onClick = button => {
+        if(button === "=") {
+          this.calculate();
+        }
+    
+        else if(button === "C") {
+          this.reset();
+        }
+    
+        else if(button === "CE") {
+          this.backspace();
+        }
+    
+        else {
+          this.setState({
+            result: this.state.result + button
+          })
+        }
+      };
+    
+      calculate = () => {
+        let { result } = this.state;
+        const calculatedResult = eval(result);
+        this.setState({
+            result: calculatedResult.toString()
+        });
+    };
+    
+      reset = () => {
+        this.setState({
+          result: ""
+        })
+      };
+    
+      backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+      };
+
+      render() {
+        return (
+          <div>
+            <div className="calculator-body">
+              <Result result={this.state.result} />
+              <Keypad onClick={this.onClick} />
+            </div>
+          </div>
+        )
+      }
+    
+}
+
+export default MainCal;
